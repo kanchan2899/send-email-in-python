54158e675bcd216b4119cf455a983ac56123ee78
kanchan2899 <kanchan2899@gmail.com>
2019-11-02 21:55:20 +0000

    Adding sene_email.py

---
+++ send_email.py
@@ -0,0 +1,8 @@
+import smtplib
+conn = smtplib.SMTP('smtp.gmail.com', 587)
+type(conn)
+conn.ehlo()
+conn.starttls()
+conn.login('emailed', 'password')
+conn.sendemail('sender's-emailid', 'receiver's-emailed', 'Subject: Test Email\n\nHey This is email shoot from a python program\n\n-Kanchan')
+conn.quit()
